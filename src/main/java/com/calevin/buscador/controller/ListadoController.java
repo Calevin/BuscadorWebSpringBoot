7364112486155d14653d3f3f08f1472398bc78ef
Sebastian Barreto <sebastian.e.barreto@gmail.com>
2021-06-13 04:07:37 -0300

    Pagina Listado carga la lista usando ClubService y thymeleaf

--- src/main/java/com/calevin/buscador/controller/ListadoController.java
+++ src/main/java/com/calevin/buscador/controller/ListadoController.java
@@ -1,13 +1,25 @@
 package com.calevin.buscador.controller;
 
+import com.calevin.buscador.models.Club;
+import com.calevin.buscador.services.ClubService;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
 
 @Controller
 public class ListadoController {
 
-    @GetMapping("/")
-    public String listarVidejuegos(){
+    private final ClubService clubService;
+
+    public ListadoController(ClubService clubService) {
+        this.clubService = clubService;
+    }
+
+    @RequestMapping("/")
+    public String listarVidejuegos(Model model){
+        model.addAttribute("clubes", this.clubService.obtenerClubes());
         return "listado";
     }
 }
